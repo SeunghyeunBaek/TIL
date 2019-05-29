02d3efb4c415c9b5e3a60cb3ddb389da892667bf
Seunghyeun Baek <wonca14@ajou.ac.kr>
2019-05-29 15:23:30 +0900

    how_namy_lotto

--- 7_웹개발/3_flask/app.py
+++ 7_웹개발/3_flask/app.py
@@ -19,9 +19,6 @@ def pick_lotto():
     lucky_numbers.sort()  # list is mutable class, list 원본을 바꾼다. Tuple, str 에는 없다.
     return render_template('./pick_lotto.html', lucky_numbers=lucky_numbers)  # html에 변수 넘기기
 
-@app.route('/get_lotto')
-def get_lotto():
-
 # variable routing : 번수처럼 url 값을 입력받는다.
 @app.route('/get_win_numbers/<int:round_number>')
 def win_num(round_number):
@@ -30,46 +27,47 @@ def win_num(round_number):
     lotto_json = json.loads(lotto_data)
     # 최대한 fail 일 때 연산을 안하도록 만든다.
     if lotto_json['returnValue'] == 'fail':
-        return f'{round_number}회차는 아직 안열렸다'
+        return f'<h1>{round_number}회차는 아직 안열렸다</h1>'
     else:
-        keys = lotto_json.keys()
         # 회차 정보
         round_number = round_number
         round_date = lotto_json['drwNoDate']
         # 당첨 번호
         win_numbers = [value for key, value in lotto_json.items() if 'drwtNo' in key]
         win_numbers.sort()
+        # keys = lotto_json.keys()
         # win_number_keys = [num_key for num_key in keys if 'drwt' in num_key]
         # win_numbers = [lotto_json[win_number_key] for win_number_key in win_number_keys]
         # win_numbers.sort()
-        # 보너스 번호
-        bonus_number = lotto_json['bnusNo']
-        # 내가 찍은 숫자
-        unlucky_numbers = random.sample(range(1,46), 6)
+        bonus_number = lotto_json['bnusNo']  # 보너스 번호
+        unlucky_numbers = random.sample(range(1, 46), 6)  # 내가 찍은 숫자
         unlucky_numbers.sort()
+        zip_numbers = zip(unlucky_numbers, win_numbers)
         # 등수 산출
-        count = len(list(set(win_numbers).intersection(unlucky_numbers)))  # 맞춘 수
-        bonus_count = 1 if bonus_number in unlucky_numbers else 0  # 보너스 번호
+        count = len(set(win_numbers).intersection(unlucky_numbers))  # 맞춘 번호 개수
+        bonus_count = 1 if bonus_number in unlucky_numbers else 0  # 보너스 번호(맞추면 1 틀리면 0)
 
         if count == 6:
             rank = 1
         elif count == 5 and bonus_count == 1:
             rank = 2
-        elif count == 5 and bonus_count == 0:
+        elif count == 5:
             rank = 3
         elif count == 4:
             rank = 4
         elif count == 3:
             rank = 5
         else:
-            rank = -1
+            rank = '언랭'
 
         return render_template('./get_win_numbers.html',
                                win_numbers=win_numbers,
                                bonus_number=bonus_number,
-                               rank=rank, count=count,
+                               rank=rank,
+                               count=count,
                                bonus_count=bonus_count,
                                unlucky_numbers=unlucky_numbers,
+                               zip_numbers=zip_numbers,
                                round_number=round_number,
                                round_date=round_date)
 
@@ -130,6 +128,7 @@ def how_many_lotto(round_number):
                            wasted_money=wasted_money,
                            round_number=round_number,
                            round_date=round_date)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
