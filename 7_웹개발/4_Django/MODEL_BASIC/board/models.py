8e5181cd1afe9d796cb06c0c580a40acd7073ea5
Seunghyeun Baek <wonca14@ajou.ac.kr>
2019-06-04 17:24:33 +0900

    MODEL_BASIC | 190604

--- 7_웹개발/4_Django/MODEL_BASIC/board/models.py
+++ 7_웹개발/4_Django/MODEL_BASIC/board/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return f'{self.id} : {self.title} - {self.content}'
